d53c96398849190e515adacb84cd2d2f2cf942cf
Julius Streit <julius.streit@student.hpi.de>
2019-11-18 23:18:12 +0100

    fixes for linux native

--- measure-disk-random-read.sh
+++ measure-disk-random-read.sh
@@ -5,6 +5,6 @@ fi
 
 FIORESULT=$(fio --name=randomread --ioeng=posixaio --runtime=15 --time_based --readwrite=randread --size=500M --iodepth=4 --direct=1 > fioread.txt)
 IOPLINE=$(grep iops fioread.txt)
-RESULT=$( echo $IOPLINE | sed -e 's/.*avg=\(.*\)stdev.*/\1/' | sed 's/[ \t]*$//' | sed s/,*\r*$//)
+RESULT=$( echo $IOPLINE | sed -e 's/.*iops=\(.*\)runt.*/\1/' | sed 's/[ \t]*$//' | sed s/,*\r*$//)
 rm randomread*
 echo $RESULT
