60a4f17225acf61f2a501e3eeb6811a460e6b74c
Jona Otholt <18723283+J-L-O@users.noreply.github.com>
2019-11-18 22:49:37 +0100

    Add cpu and sequential benschmarks to benchmarking script

--- benchmarking.sh
+++ benchmarking.sh
@@ -4,6 +4,16 @@ if [ ! -e $FILE ] ; then
 	$( echo "time,value" >> $FILE)
 fi
 
+RESULT=$(./measure-cpu.sh)
+DATE=$(date +%s)
+echo $DATE,$RESULT >> $FILE
+
+FILE="mem.csv"
+if [ ! -e $FILE ] ; then
+	touch $FILE
+	$( echo "time,value" >> $FILE)
+fi
+
 RESULT=$(./measure-mem.sh)
 DATE=$(date +%s)
 echo $DATE,$RESULT >> $FILE
@@ -25,6 +35,26 @@ if [ ! -e $FILE ] ; then
 fi
 
 RESULT=$(./measure-disk-random-write.sh)
+DATE=$(date +%s)
+echo $DATE,$RESULT >> $FILE
+
+FILE="disk-sequential-write.csv"
+if [ ! -e $FILE ] ; then
+	touch $FILE
+	$( echo "time,value" >> $FILE)
+fi
+
+RESULT=$(./measure-disk-sequential-write.sh)
+DATE=$(date +%s)
+echo $DATE,$RESULT >> $FILE
+
+FILE="disk-sequential-read.csv"
+if [ ! -e $FILE ] ; then
+	touch $FILE
+	$( echo "time,value" >> $FILE)
+fi
+
+RESULT=$(./measure-disk-sequential-read.sh)
 DATE=$(date +%s)
 echo $DATE,$RESULT >> $FILE
 
