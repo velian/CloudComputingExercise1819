2ac16b6225aeb3289f85064c6ff01a43a8725b6a
Jona Otholt <18723283+J-L-O@users.noreply.github.com>
2019-12-09 21:10:49 +0000

    Finalize Nginx benchmark

--- Assignment2/measure-nginx.sh
+++ Assignment2/measure-nginx.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 # Execute this on the client
-result_string=$(wget $1/output.dat)
-result=$(echo $result_string | sed -n "s/^.*in\s*\([0-9]*\).*$/\1/p")
+result_string=$(wget $1/output.dat 2>&1)
+
+result=$(echo $result_string | sed -n "s/^.*=\(\S*\)s.*$/\1/p")
 
 rm output.dat
 
