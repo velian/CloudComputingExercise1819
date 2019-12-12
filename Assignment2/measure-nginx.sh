e7ff6079a3948b03dd1776152080ef7ef03cbe17
Jona Otholt <18723283+J-L-O@users.noreply.github.com>
2019-12-12 11:18:43 +0100

    Change file name

--- Assignment2/measure-nginx.sh
+++ Assignment2/measure-nginx.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Execute this on the client
-result_string=$(wget -O output1.dat $1/output.dat 2>&1)
+result_string=$(wget -O output.dat $1/output.dat 2>&1)
 
 result=$(echo $result_string | sed -n "s/^.*=\(\S*\)s.*$/\1/p")
 
